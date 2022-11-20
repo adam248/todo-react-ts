5b76aa928e290a3e18285879f31a4c721e5283e2
Adam Butler <adamjbutler091@gmail.com>
2022-11-20 17:01:36 +0930

    display todo list and done edit delete btns

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const CHECK = "✔️"
+const TRASH = "🗑️"
+const PEN = "🖊️"
+
 function App() {
+
+  // Tasks (Todo List) State
+
+  const [toDo, setToDo] = useState([
+    {"id": 1, "title": "Task 1", "status": true},
+    {"id": 2, "title": "Task 2", "status": false}
+  ]);
+  
+  // Temp State
+  const [newTask, setNewTask] = useState('');
+  const [changedTask, setChangedTask] = useState('');
+
+  // Add Task
+  const addTask = () => {
+    //
+  }
+  // Delete Task
+  const deleteTask = () => {
+    //
+  }
+  // Marktask as done or completed
+  const markDone = () => {
+    //
+  }
+  // Cancel update
+  const cancelUpdate = () => {
+    //
+  }
+  // Change Task
+  const changeTask = () => {
+    //
+  }
+  // Update task
+  const updateTask = () => {
+    //
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+      <br></br>
+      <h2>To Do List App</h2>
+      <br></br>
+
+    {/* Display ToDos*/}
+    {toDo && toDo.length ? '' : 'No Tasks...'}
+
+    {toDo && toDo
+    .sort((a, b) => a.id > b.id ? 1 : -1)
+    .map((task, index) => {
+      return (
+      <React.Fragment key={task.id}>
+        <div className="col taskBg">
+          <div className={task.status ? 'done' : ''}>
+            <span className="taskNumber">{task.id}</span>
+            <span className="taskText">{task.title}</span>
+          </div>
+          <div className="iconsWrap">
+            <span title="Completed / Not Completed">
+              {CHECK}
+            </span>
+            <span title="Edit">
+              {PEN}
+            </span>
+            <span title="Delete">{TRASH}</span>
+          </div>
+        </div>
+       </React.Fragment> 
+        )
+    })}
     </div>
-  );
+ );
 }
 
 export default App;
